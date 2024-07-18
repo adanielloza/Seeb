8b22b984f38c6590e8de2cf4b00c9d6e4ff3119d
adanielloza <123408012+adanielloza@users.noreply.github.com>
2024-07-18 17:44:03 -0500

    vite update

--- vite-project/src/controllers/TaskController.js
+++ vite-project/src/controllers/TaskController.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 import { Task } from '../models/Task';
 
-const API_URL = 'http://127.0.0.1:5000/api/tasks';
+const API_URL = 'https://seeb-5.onrender.com/api/tasks';
 
 export const getTasks = async () => {
     const response = await axios.get(API_URL);
